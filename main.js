4e9f10180db531fd4e5b2610d53e1079fa45917f
SinRopa <bsorense@kent.edu>
2019-04-27 14:50:25 -0400

    Cleaning pasta files

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 
-function autoSave() {
-    save();
-    setTimeout(autoSave, 60000);
-}
-
 var isSaving = false;
 
 function save() {
@@ -11,34 +6,6 @@ function save() {
     var saveString = JSON.stringify(game);
     var saveGame = JSON.parse(saveString);
 	isSaving = false;
-  //delete anything from game that doesnt need to be saved to save space
-  
-  
-  //compress whats left to save more space
-  saveString = LZString.compressToBase64(JSON.stringify(saveGame));
-  
-  	try
-    {
-		  localStorage.setItem("WOTN",saveString);
-		  if (localStorage.getItem("WOTN") == saveString)
-      {
-			//Game Saved
-		  }
-		  else 
-      {
-			//bad save attempt
-		  }
-	 }
-	catch(e)
-  {
-		if(e.name == "NS_ERROR_FILE_CORRUPTED") 
-    {
-        //need to clear browser cache
-		}
-		else
-		//not saving for some other reason
-  }
-} //end save
   
   function load()
   {
@@ -46,11 +13,11 @@ function save() {
     try 
     {
       
-      var loadsave = localStorage.getItem("WOTN");
+      var loadsave = localStorage.getItem("MC");
     }
     catch(e) {}
     
-    if (loadsave !== null) savedata = JSON.parse(LZString.decompressFromBase64(loadsave));
+    if (loadsave !== null) savedata = JSON.parse(loadsave);
     
     //update game with loaded values
   }
